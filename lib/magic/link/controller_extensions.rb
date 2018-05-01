433ce112c95b933d52c2a8fb084247172b93f895
David Van Der Beek <earlynovrock@gmail.com>
2018-05-01 13:02:22 -0500

    token expiration

--- lib/magic/link/controller_extensions.rb
+++ lib/magic/link/controller_extensions.rb
@@ -10,10 +10,21 @@ module Magic
           email = params[:email].presence
           user  = email && Magic::Link.user_class.find_by(email: email)
 
-          if user && Devise.secure_compare(user.sign_in_token, Devise.token_generator.digest(Magic::Link.user_class, :sign_in_token, params[:sign_in_token]))
+          if user && token_matches?(user) && token_not_expired?
             user.update_columns(sign_in_token: nil, sign_in_token_sent_at: nil)
             sign_in user
           end
+        end
+
+        def token_mathces?(user)
+          Devise.secure_compare(
+            user.sign_in_token,
+            Devise.token_generator.digest(Magic::Link.user_class, :sign_in_token, params[:sign_in_token])
+          )
+        end
+
+        def token_not_expired?
+          user.sign_in_token_sent_at >= 6.seconds.ago
         end
       end
     end
