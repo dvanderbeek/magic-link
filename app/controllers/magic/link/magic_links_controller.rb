0883d4e05d22e81608443f6d0d5a9562c425d04a
Jesse Thomas Hanley <jesse@jessehanley.com>
2020-07-21 17:07:32 +0900

    Skip raising as a workaround

--- app/controllers/magic/link/magic_links_controller.rb
+++ app/controllers/magic/link/magic_links_controller.rb
@@ -1,7 +1,7 @@
 module Magic
   module Link
     class MagicLinksController < ::ApplicationController
-      skip_before_action :authenticate_user!
+      skip_before_action :authenticate_user!, :raise => false
       before_action :check_user, only: :new
 
       def new
